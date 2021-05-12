e1a92a7427d9cca7fb78c9165ba81349c17024ea
dvicjon <devinvjones@gmail.com>
2021-05-12 18:18:35 -0400

    cart item component

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+//import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
-import { QUERY_CHECKOUT } from '../../utils/queries';
+//import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/react-hooks';
+//import { useLazyQuery } from '@apollo/react-hooks';
 import { useDispatch, useSelector } from 'react-redux';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
