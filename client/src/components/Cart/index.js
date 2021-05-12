38364c9cb37a1621edf7e3fae6bde321788a25ff
dvicjon <devinvjones@gmail.com>
2021-05-12 18:08:20 -0400

    cart component

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,110 @@
+import React, { useEffect } from 'react';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { useDispatch, useSelector } from 'react-redux';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+const Cart = () => {
+
+    const state = useSelector((state) => {
+      return state; 
+    })
+
+    const dispatch = useDispatch(); 
+    
+    //const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        async function getCart() {
+          const cart = await idbPromise('cart', 'get');
+          //dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+      
+        if (!state.cart.length) {
+          getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
+    function toggleCart() {
+        //dispatch({ type: TOGGLE_CART });
+    }
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+          sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productIds = [];
+      
+        state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+    }
+    useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+    }, [data]);
+
+    if (!state.cartOpen) {
+        return (
+          <div onClick={toggleCart}>
+            <span
+              role="img"
+              aria-label="sunflower">🌻</span>
+          </div>
+        );
+    }
+
+  return (
+<div>
+  <div onClick={toggleCart}>[close]</div>
+  <h2>Cart</h2>
+  {state.cart.length ? (
+    <div>
+      {state.cart.map(item => (
+        <CartItem key={item._id} item={item} />
+      ))}
+      <div>
+        <strong>Seeds: {calculateTotal()}</strong>
+        {
+          Auth.loggedIn() ?
+            <button onClick={submitCheckout}>
+              Checkout
+            </button>
+            :
+            <span>(log in to check out)</span>
+        }
+      </div>
+    </div>
+  ) : (
+    <h3>
+      <span role="img" aria-label="sad flower">
+      🥀
+      </span>
+      Cart empty!
+    </h3>
+  )}
+</div>
+  );
+};
+
+export default Cart;
