6998d776e627096339cbe03381f5428cbbd27044
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-12 17:15:01 -0400

    working on graphql typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,26 @@
 //code needed
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+  }
+
+  type Product {
+
+  }
+
+  type Auth {
+
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
+`
