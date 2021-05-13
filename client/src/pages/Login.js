3b7ad10c93c9db4713e60293d6541d857f78f639
Kristen Groller <groller@rogers.com>
2021-05-12 21:42:51 -0400

    login page updated and signup page started

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 // import { useMutation } from '@apollo/react-hooks';
 // // import { Link } from "react-router-dom";
 // import { LOGIN } from "../utils/mutations"
@@ -14,34 +14,56 @@ import {
   Button
 } from '@chakra-ui/react';
 
-function Login(props) {
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`Email: ${email} & Password: ${password}`);
+  };
+
+
 
   return (
 
-        <Flex width="full" align="center" justifyContent="center">
-          <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
-            <Box textAlign="center">
-              <Heading>Login</Heading>
-              </Box>
-              <Box my={4} textAlign="left">
-               
-          <form>
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" placeholder="test@test.com" />
-            </FormControl>
-            <FormControl mt={6}>
-              <FormLabel>Password</FormLabel>
-              <Input type="password" placeholder="*******" />
-            </FormControl>
-            <Button width="full" mt={4} type="submit">
-              Sign In
-            </Button>
-          </form>
-            </Box>
-          </Box>
-        </Flex>
-      );
-  }
+    <Flex width="full" align="center" justifyContent="center">
+      <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+        <Box textAlign="center">
+          <Heading>Login</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
 
-export default Login;
+          <form onSubmit={handleSubmit}>
+            <FormControl isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                placeholder="test@test.com"
+                size="lg"
+                onChange={event => setEmail(event.currentTarget.value)}
+              />
+            </FormControl>
+            <FormControl isRequired mt={6}>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                placeholder="*******"
+                size="lg"
+                onChange={event => setPassword(event.currentTarget.value)}
+              />
+            </FormControl>
+            <Button
+              variantColor="teal"
+              variant="outline"
+              type="submit"
+              width="full"
+              mt={4}
+            >
+              Sign In
+  </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,80 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 // import { useMutation } from '@apollo/react-hooks';
+// // import { Link } from "react-router-dom";
+// import { LOGIN } from "../utils/mutations"
 // import Auth from "../utils/auth";
+
+import {
+    Flex,
+    Box,
+    Heading,
+    FormControl,
+    FormLabel,
+    Input,
+    Button
+} from '@chakra-ui/react';
+
+export default function Signup() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = event => {
+        event.preventDefault();
+        alert(`Username: ${username} & Email: ${email} & Password: ${password}`);
+    };
+
+
+
+    return (
+
+        <Flex width="full" align="center" justifyContent="center">
+            <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+                <Box textAlign="center">
+                    <Heading>Signup</Heading>
+                </Box>
+                <Box my={4} textAlign="left">
+
+                    <form onSubmit={handleSubmit}>
+
+                        <FormControl isRequired>
+                            <FormLabel>Username</FormLabel>
+                            <Input
+                                type="username"
+                                placeholder="*******"
+                                size="lg"
+                                onChange={event => setUsername(event.currentTarget.value)}
+                            />
+                        </FormControl>
+                        <FormControl isRequired>
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                type="email"
+                                placeholder="test@test.com"
+                                size="lg"
+                                onChange={event => setEmail(event.currentTarget.value)}
+                            />
+                        </FormControl>
+                        <FormControl isRequired mt={6}>
+                            <FormLabel>Password</FormLabel>
+                            <Input
+                                type="password"
+                                placeholder="*******"
+                                size="lg"
+                                onChange={event => setPassword(event.currentTarget.value)}
+                            />
+                        </FormControl>
+                        <Button
+                            variantColor="teal"
+                            variant="outline"
+                            type="submit"
+                            width="full"
+                            mt={4}
+                        >
+                            Sign In
+                        </Button>
+                    </form>
+                </Box>
+            </Box>
+        </Flex>
+    );
+};
