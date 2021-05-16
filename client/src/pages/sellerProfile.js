12caf73e28db51042524241a35bb285079ff899d
raedaltaki <raed.simon@gmail.com>
2021-05-16 08:17:13 -0400

    add seller profile

--- client/src/pages/sellerProfile.js
+++ client/src/pages/sellerProfile.js
@@ -3,14 +3,14 @@ import React from "react";
 import { Link as ReactLink } from "react-router-dom";
 import { Box, Image, Flex, Text, Divider, Stack, Center, SimpleGrid, Link, } from '@chakra-ui/react';
 import { useQuery } from '@apollo/react-hooks';
-// import { QUERY_BUYER } from "../utils/queries";
+import { QUERY_SELLER } from "../utils/queries";
 
 function buyerProfile() {
-    // const { data } = useQuery(QUERY_BUYER);
+    // const { data } = useQuery(QUERY_SELLER);
     let user;
 
     // if (data) {
-    //     user = data.buyer;
+    //     user = data.seller;
     // }
 
     user={
@@ -30,7 +30,7 @@ function buyerProfile() {
                         name:"apple",
                         description:"",
                         image:"",
-                        quantity:"",
+                        quantity: 2,
                         price: 30
                     },
                     {
@@ -38,7 +38,7 @@ function buyerProfile() {
                         name:"apple",
                         description:"",
                         image:"",
-                        quantity:"",
+                        quantity:4,
                         price: 30
                     }
                 ],
@@ -53,14 +53,34 @@ function buyerProfile() {
                     {
                         _id:"3",
                         name:"apple",
-                        description:"",
+                        description:"kkkkkkk",
                         image:"",
-                        quantity:"",
+                        quantity:5,
                         price: 30
                     }
                 ],
                 sellerId: "4",
                 buyerId: "1"
+            }
+        ],
+        sales:
+        [
+            {
+                _id: 5,
+                purchaseDate: 1621100000000,
+                products:
+                [
+                    {
+                        _id: "6",
+                        name: "banana",
+                        description: "hhhhh",
+                        image: "",
+                        quantity: 8,
+                        price: 20
+                    }
+                ],
+                sellerId:1,
+                buyerId:9
             }
         ],
         seeds :50
@@ -71,7 +91,7 @@ function buyerProfile() {
             <Box>
                 <Flex height="100hv" alignItems="top" justifyContent="space-between">  
                     <Box>   
-                        <Text m={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                        <Text m={2} fontSize="xx-large" fontWeight="semibold" lineHeight="short">
                             Profile for {user.firstName} {user.lastName}
                         </Text>
                         <Text m={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
@@ -84,12 +104,12 @@ function buyerProfile() {
                 </Flex>
             </Box>
             <Divider orientation="horizontal" />
-            <Flex height="100hv" alignItems="center" justifyContent="center">
-                <Box width="100%" textAlign="center">
-                   <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+            <Flex height="100hv" alignItems="top" justifyContent="center">
+                <Box width="50%" textAlign="center">
+                   <Text mt="20px" fontSize="xl" fontWeight="semibold" lineHeight="short">
                         Order History
                     </Text>
-                    <Box border="1px" width="90%" padding="10px" my="20px" mx="auto" textAlign="center">
+                    <Box border="1px" width="90%" minHeight="400px" padding="10px" my="20px" mx="auto" textAlign="center">
                         {/* <orderHistory/> */}
                         {user.purchases.map((order) => (
                             <Box key={order._id} m="10px" fontSize="xl" fontWeight="semibold" lineHeight="short">
@@ -97,7 +117,39 @@ function buyerProfile() {
                                     {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
                                 </Text>
                                 <Box d="flex" alignItems="baseline">
-                                    {order.products.map(({ _id, image, name, price, quantity }, index) => (
+                                    {order.products.map(({ _id, image, name, quantity ,price }, index) => (
+                                        <Box key={index} padding="4" m="10px" bg="gray.100" maxW="3xl">
+                                            <Link as={ReactLink} to={`/products/${_id}`}>
+                                                <Image
+                                                alt={name}
+                                                src={`/images/${image}`}
+                                                />
+                                                <Box>{name} Qty:{quantity}</Box>
+                                            </Link>
+                                            <Box>
+                                                <Text>${price}</Text>
+                                            </Box>
+                                        </Box>
+                                    ))}         
+                                </Box>
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+
+                <Box width="50%" textAlign="center">
+                    <Text mt="20px" fontSize="xl" fontWeight="semibold" lineHeight="short">
+                        Selling History
+                    </Text>
+                    <Box border="1px" width="90%" minHeight="400px" padding="10px" my="20px" mx="auto" textAlign="center">
+                        {/* <orderHistory/> */}
+                        {user.sales.map((order) => (
+                            <Box key={order._id} m="10px" fontSize="xl" fontWeight="semibold" lineHeight="short">
+                                <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+                                    {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                                </Text>
+                                <Box d="flex" alignItems="baseline">
+                                    {order.products.map(({ _id, image, name,quantity, price }, index) => (
                                         <Box key={index} padding="4" m="10px" bg="gray.100" maxW="3xl">
                                             <Link as={ReactLink} to={`/products/${_id}`}>
                                                 <Image
