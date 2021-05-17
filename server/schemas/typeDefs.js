8f8cc7f30c0cdfc00e0ba3287fef9caa98d0cdab
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-17 15:14:40 -0400

    created addProduct mutation in typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -63,6 +63,7 @@ const typeDefs = gql`
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
+    addProduct(name: String!, description: String!, price: Float!, quantity: Int!, category: ID!): Product
     createProduct(productInfo: NewProduct!): User
     deleteProduct(productId: ID!): User
   }
