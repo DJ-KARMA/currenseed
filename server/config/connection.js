91df085f5ec6506d5f14986016154f53ce7b9346
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-20 21:45:40 -0400

    typo corrections

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hayleyvuylsteke:RosieRaisin2@cluster0.ddcpa.mongodb.net/currenseed?retryWrites=true&w=majority', 
-{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/currenseed', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
