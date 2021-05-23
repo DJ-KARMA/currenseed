86624d00a133714662b4614f9a90cc09d9880434
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-05-22 22:32:22 -0400

    trying to resolve service worker/pwa issues

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -16,4 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-<script>serviceWorker.register();</script>
+serviceWorkerRegistration.register();
